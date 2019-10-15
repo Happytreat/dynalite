864b892d22dd0a1d06303a91c013b5c0e6081cae
DigiPie <evan.it.sg@gmail.com>
2019-10-15 20:39:18 +0800

    Move backend-http rcode into src

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var dummy_data = [
+    { type: "Confirmable(0)", id: "57602", payload: 
+      {"timestamp":1571142032134,"light_id":"1","light_status":0}
+    },
+    { type: "Non-confirmable(1)", id: "32331", payload: 
+      {"timestamp":1571142113376,"light_id":"2","light_status":1}
+    },
+    { type: "Acknowledgement (2)", id: "57358", payload: 
+      {"timestamp":1571142118639,"light_id":"1","light_status":0}
+    },
+    { type: "Reset (3)", id: "30969", payload: 
+      {"timestamp":1571142122891,"light_id":"2","light_status":1}
+    }
+  ];
+
+  res.render('index', { 
+    data: dummy_data
+  });
 });
 
 module.exports = router;
