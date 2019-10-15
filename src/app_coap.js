8630ae01afe7ffb5e806b8b4b81cd0e5634a3ba3
DigiPie <evan.it.sg@gmail.com>
2019-10-15 21:48:06 +0800

    Shift COAP middleware from bin/www to app_coap.js

--- src/app_coap.js
+++ src/app_coap.js
@@ -2,6 +2,19 @@ const router = require("coap-router");
 const app_coap = router();
 
 app_coap.get("/", (req, res) => {
+    var type;
+    if (req._packet.reset) {
+        type = "Reset (3)";
+    } else if (req._packet.ack) {
+        type = "Acknowledgement (2)";
+    } else if (req._packet.confirmable) {
+        type = "Confirmable (0)";
+    } else {
+        type = "Non-confirmable (1)";
+    }
+  
+    console.log("COAP " + req.method + " " + type + ": " + req._packet.messageId + ": " + req._packet.payload);
+
     res.end("Hello COAP");
 });
 
