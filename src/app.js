3b096810e155eb003760e9c864d7270997fc7e67
DigiPie <evan.it.sg@gmail.com>
2019-10-15 22:25:12 +0800

    Add PostgreSQL implementation for HTTP web page The HTTP web page at '/' will render COAP messages stored in PostgreSQL

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
+
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 
-var app = express();
-
 // view engine setup
+var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+var sql_query = 'SELECT * FROM coap_post';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var dummy_data = [
-    { type: "Confirmable(0)", id: "57602", payload: 
-      {"timestamp":1571142032134,"light_id":"1","light_status":0}
-    },
-    { type: "Non-confirmable(1)", id: "32331", payload: 
-      {"timestamp":1571142113376,"light_id":"2","light_status":1}
-    },
-    { type: "Acknowledgement (2)", id: "57358", payload: 
-      {"timestamp":1571142118639,"light_id":"1","light_status":0}
-    },
-    { type: "Reset (3)", id: "30969", payload: 
-      {"timestamp":1571142122891,"light_id":"2","light_status":1}
-    }
-  ];
-
-  res.render('index', { 
-    data: dummy_data
-  });
+  pool.query(sql_query, (err, data) => {
+    console.log(err);
+		res.render('index', { 
+			data: data.rows 
+		});
+	});
 });
 
 module.exports = router;
