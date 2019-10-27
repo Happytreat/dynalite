e12303d353048c214a4c31f38607dba4ba67a546
Happytreat <cowfats@gmail.com>
2019-10-27 20:07:38 +0800

    Add more dummy data for the week

--- src/seeders/occupancy.js
+++ src/seeders/occupancy.js
@@ -4,14 +4,14 @@ const createFakeOccupancy = async (Occupancy) => {
     return Occupancy.create(
         {
             rpiId: faker.random.uuid(),
-            timestamp: faker.date.between('2019-10-26', '2019-10-27'),
+            timestamp: faker.date.between('2019-10-21', '2019-10-27'),
             isOccupied: faker.random.boolean()
         }
       )
 }
 
 export const seedOccupancyTable = async (Occupancy) => {
-    const numberOfData = 5;
+    const numberOfData = 1000;
     const _ = Array.apply(null, Array(numberOfData)).map(function () {});
     return Promise.all(_.map(() => createFakeOccupancy(Occupancy)))
 }
