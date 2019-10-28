a9c938984511fa1ed260f394949bcbc8fbe4bf8d
DigiPie <evan.it.sg@gmail.com>
2019-10-28 12:54:28 +0800

    Update style and display for secrets.js

--- src/util/secrets.js
+++ src/util/secrets.js
@@ -1,17 +1,30 @@
-import dotenv from "dotenv";
-import fs from "fs";
+/*
+ * Module dependencies.
+ */
+const dotenv = require('dotenv');
+import fs from 'fs';
+/*
+ * Setup
+ */
+/* Colours */
+const cRed = '\x1b[31m';
+const cGreen = '\x1b[32m';
+const cYellow = '\x1b[33m';
+const cReset = '\x1b[0m'; // Resets the console colour
 
 if (fs.existsSync(".env")) {
-  console.log("Using .env file to supply config environment variables");
-  dotenv.config({ path: ".env" });
+  console.log(`${cYellow}[app] Attempting to locate '.env' file.${cReset}`);
+  dotenv.config({ path: '.env' });
 } else {
-  console.error("No .env file found!");
+  console.error(`${cRed}[app] No '.env' file found.${cReset}`);
 }
 
-export const ENVIRONMENT = process.env.NODE_ENV || "dev"; // Default to dev
+export const ENVIRONMENT = process.env.NODE_ENV || 'dev'; // Default to dev
 
-export const DB_URI = process.env.DATABASE_URL || "";
+export const DB_URI = process.env.DATABASE_URL || '';
 if (!DB_URI) {
-  console.error("DATABASE_URL is not set");
+  console.error(`${cRed}[app] DATABASE_URL is not set in '.env'.${cReset}`);
   process.exit(1);
 }
+
+console.log(`${cGreen}[app] Using '.env' file for environment variables.${cReset}`);
