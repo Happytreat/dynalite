7fc8230588501ef8bec8bb793b1664369d5af57e
DigiPie <evan.it.sg@gmail.com>
2019-11-09 18:01:29 +0800

    Update build

---
+++ frontend/build/precache-manifest.bb5ebcdbc488a5d749b2f2bbc9b92f63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3876b782c7959c0168bcdeca546e82",
+    "url": "https://www.evantay.com/tech/dynalite/index.html"
+  },
+  {
+    "revision": "b0a0e09e51c573730f59",
+    "url": "https://www.evantay.com/tech/dynalite/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "1d0885cb41b9b7abd34d",
+    "url": "https://www.evantay.com/tech/dynalite/static/css/main.6e8505c4.chunk.css"
+  },
+  {
+    "revision": "b0a0e09e51c573730f59",
+    "url": "https://www.evantay.com/tech/dynalite/static/js/2.843c26f2.chunk.js"
+  },
+  {
+    "revision": "1d0885cb41b9b7abd34d",
+    "url": "https://www.evantay.com/tech/dynalite/static/js/main.bfdf032e.chunk.js"
+  },
+  {
+    "revision": "d07ea4782478fd2af974",
+    "url": "https://www.evantay.com/tech/dynalite/static/js/runtime-main.5e4eb7e6.js"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://localhost:3000/precache-manifest.ce89ded0e66119c7cfe680ad80d05fc1.js"
+  "https://www.evantay.com/tech/dynalite/precache-manifest.bb5ebcdbc488a5d749b2f2bbc9b92f63.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3000/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://www.evantay.com/tech/dynalite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
