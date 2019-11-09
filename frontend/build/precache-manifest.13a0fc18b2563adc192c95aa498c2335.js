a79377085a00605f0544e675c4fcb2b8504fb62c
DigiPie <evan.it.sg@gmail.com>
2019-11-09 17:36:02 +0800

    Update build for frontend

---
+++ frontend/build/precache-manifest.13a0fc18b2563adc192c95aa498c2335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa24785ebaf5dbeedfd61c6254adf6c",
+    "url": "https://www.evantay.com/tech/dynalite/index.html"
+  },
+  {
+    "revision": "b0a0e09e51c573730f59",
+    "url": "https://www.evantay.com/tech/dynalite/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "5b83b414d99baf6a69ff",
+    "url": "https://www.evantay.com/tech/dynalite/static/css/main.6e8505c4.chunk.css"
+  },
+  {
+    "revision": "b0a0e09e51c573730f59",
+    "url": "https://www.evantay.com/tech/dynalite/static/js/2.843c26f2.chunk.js"
+  },
+  {
+    "revision": "5b83b414d99baf6a69ff",
+    "url": "https://www.evantay.com/tech/dynalite/static/js/main.42b608e9.chunk.js"
+  },
+  {
+    "revision": "d07ea4782478fd2af974",
+    "url": "https://www.evantay.com/tech/dynalite/static/js/runtime-main.5e4eb7e6.js"
+  }
+]);
