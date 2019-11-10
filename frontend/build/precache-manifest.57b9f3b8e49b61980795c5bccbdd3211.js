4d005690c2ff3c5bb95c35f45ed6ad29f832a728
DigiPie <evan.it.sg@gmail.com>
2019-11-10 12:40:02 +0800

    Update build

--- frontend/build/precache-manifest.57b9f3b8e49b61980795c5bccbdd3211.js
+++ frontend/build/precache-manifest.57b9f3b8e49b61980795c5bccbdd3211.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "908ef722ed1b241d12f9d9c44434982c",
+    "revision": "2ce92a02256244bbf61ebde763a40964",
     "url": "https://www.evantay.com/tech/dynalite/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://www.evantay.com/tech/dynalite/static/css/2.aa7fcd32.chunk.css"
   },
   {
-    "revision": "a007f622f7a79a937ef0",
+    "revision": "578616234b6a1c792ea1",
     "url": "https://www.evantay.com/tech/dynalite/static/css/main.6e8505c4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://www.evantay.com/tech/dynalite/static/js/2.4b1b67c7.chunk.js"
   },
   {
-    "revision": "a007f622f7a79a937ef0",
-    "url": "https://www.evantay.com/tech/dynalite/static/js/main.b35886b2.chunk.js"
+    "revision": "578616234b6a1c792ea1",
+    "url": "https://www.evantay.com/tech/dynalite/static/js/main.f2ef8662.chunk.js"
   },
   {
     "revision": "d07ea4782478fd2af974",
